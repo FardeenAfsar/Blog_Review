f96631503d56a884a635e6fcf6d25c24a6a4b8b6
Fardeen Afsar <afsarfardeen@gmail.com>
2021-05-06 14:37:49 +0600

    Set up tmdb api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const passport = require("passport");
 const Review = require("../models/Review");
 
+const axios = require("axios");
+
 const { ensureAuth, ensureGuest, validateAuth } = require("../middleware/auth");
+const { response } = require("express");
 
 // @desc    Landing Page
 // @route   GET /
@@ -60,6 +63,27 @@ router.post("/review", ensureAuth, async (req, res) => {
   req.body.user = req.user.id;
   await Review.create(req.body);
   res.redirect("/home");
+});
+
+// @desc    Post review Page
+// @route   GET /post
+router.post("/movie", ensureAuth, async (req, res) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/movie?api_key=17d664f31d975895c103aedfbd6f445c&language=en-US&query=${req.body.search}&page=1&include_adult=false`
+    )
+    .then((response) => {
+      const data = response.data.results.sort(
+        (a, b) => parseFloat(b.popularity) - parseFloat(a.popularity)
+      );
+      res.render("movie", {
+        data: data.slice(0, 11),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/home");
+    });
 });
 
 module.exports = router;
