57bab83b59c9d3adaad1444f835455853389d800
Fardeen Afsar <afsarfardeen@gmail.com>
2021-05-12 19:14:36 +0600

    Changes to nav bar

--- routes/index.js
+++ routes/index.js
@@ -92,17 +92,16 @@ router.get("/user", ensureAuth, async (req, res) => {
 });
 
 // #region playground
+// router.get("/fav", ensureAuth, async (req, res) => {
+//   res.render("fav");
+// });
 
-router.get("/fav", ensureAuth, async (req, res) => {
-  res.render("fav");
-});
-
-router.post("/pg", ensureAuth, async (req, res) => {
-  console.log(req.query.m);
-  res.render("fav", {
-    movie: req.query.m,
-  });
-});
+// router.post("/pg", ensureAuth, async (req, res) => {
+//   console.log(req.query.m);
+//   res.render("fav", {
+//     movie: req.query.m,
+//   });
+// });
 // #endregion
 
 module.exports = router;
